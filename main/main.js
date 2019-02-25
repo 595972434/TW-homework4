4914311e2e082aaa8ddbef3795d99bd48da0f213
Cas <595972434@qq.com>
2019-02-25 17:10:46 +0800

    代码重构完成

--- main/main.js
+++ main/main.js
@@ -1,14 +1,20 @@
 module.exports = function main(dis,time) {
     console.log("Debug Info");
-    if(dis<0 || time<0)
-    {   return "Invalid";}
-    else
-    {
-        if(dis<=2)
-            return Math.round(6+time*0.25);
-        else if(dis <=8)
-            return Math.round(6+(dis-2)*0.8+time*0.25);
-        else
-            return Math.round(6+(dis-2)*0.8+(dis-8)*0.4+time*0.25);
-    }
+    if(dis<0 || time<0) return "Invalid";
+    else return Math.round(DisFee(dis)+TimeFee(time));
 };
+
+/**
+ * @return {number}
+ */
+function DisFee(distance){
+    if(distance<=2) return 6;
+    else if(distance<=8) return 6+(distance-2)*0.8;
+    else return 6+(distance-2)*0.8+(distance-8)*0.4;
+}
+/**
+ * @return {number}
+ */
+function TimeFee(tim){
+     return tim*0.25;
+}
