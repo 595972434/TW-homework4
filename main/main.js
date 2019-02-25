0899f7b4d4c7592e0ff74e3f9b371eeb07e5a61a
Cas <595972434@qq.com>
2019-02-25 15:28:01 +0800

    样例2测试通过

--- main/main.js
+++ main/main.js
@@ -1,5 +1,10 @@
 module.exports = function main(dis,time) {
     console.log("Debug Info");
     if(dis<0 || time<0)
-    return "Invalid";
+    {   return "Invalid";}
+    else
+    {
+        if(dis<=2)
+            return Math.round(6+time*0.24);
+    }
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -7,4 +7,10 @@ describe('出租车计价测试', function () {
         let result=main(dis,time);
         expect(result).toEqual("Invalid");
         });
+    it("距离小于两公里时，按起步价收取6块",function(){
+        let dis=1;
+        let time=0;
+        let result=main(dis,time);
+        expect(result).toEqual(6);
+    });
 });
